a8d6d0cd1e3196cfe284a31dcf41074074cf83cc
Sceat <mr.sceat@outlook.com>
2018-03-19 13:23:43 +0200

    Working release

--- src/main/java/sct/utxo/tx/Transactions.java
+++ src/main/java/sct/utxo/tx/Transactions.java
@@ -27,6 +27,9 @@ public class Transactions {
 
 		System.out.println("Total Utxo amount :" + client.listUnspent().size());
 
+		boolean acceptAll = false;
+		boolean asked = false;
+
 		do {
 			List<OutputOverview> outputs = client.listUnspent().stream().filter(Output::getSpendable).limit(maxInput).collect(Collectors.toList());
 			Optional<BigDecimal> oAmount = outputs.stream().map(o -> (Output) o).map(Output::getAmount).reduce(BigDecimal::add);
@@ -40,17 +43,20 @@ public class Transactions {
 			toAdrr.put(addr, finalAmount); // as we consolidate outputs we don't need change adress because we are spending
 											// all coins
 
-			System.out.println("Generated=");
 			System.out.println("Will send " + finalAmount.doubleValue() + " Coins for a " + (amount.subtract(finalAmount)).doubleValue() + " Coins fee");
-			System.out.println("To adress " + addr);
-			System.out.println("Amount of unspent outputs : " + outputs.size());
+			System.out.println("To : " + addr);
 
-			if (!acceptTransaction())
+			if (!asked) {
+				acceptAll = acceptAll();
+				asked = true;
+			}
+			if (!acceptAll && !acceptTransaction())
 				return; // to avoid accepting huge/bugged fee
 
 			try {
 				String tx = client.createRawTransaction(outputs, toAdrr);
 				client.sendRawTransaction(client.signRawTransaction(tx).getHex());
+				System.out.println("\n>> [" + finalAmount.doubleValue() + " sent!]\n");
 			} catch (BitcoindException e) {
 				e.printStackTrace();
 			}
@@ -61,6 +67,12 @@ public class Transactions {
 	@SuppressWarnings("resource")
 	private boolean acceptTransaction() {
 		System.out.println("\nDo you want to comfirm the transaction ? (y/n)");
+		return new Scanner(System.in).nextLine().toLowerCase().contains("y");
+	}
+
+	@SuppressWarnings("resource")
+	private boolean acceptAll() {
+		System.out.println("\nDo you want to automatically accept all transactions instead of confirming one by one ? (y/n)");
 		return new Scanner(System.in).nextLine().toLowerCase().contains("y");
 	}
 
